aac597908e69e929565e0b41dc5ed9221ebfd5f1
Marius Niveri <mniveri@cc.systems>
2025-03-20 15:22:10 +0100

    hotfix: ignore rotation and force PORTRAIT_UP always

--- android/src/main/kotlin/com/apparence/camerawesome/cameraX/CameraXState.kt
+++ android/src/main/kotlin/com/apparence/camerawesome/cameraX/CameraXState.kt
@@ -417,15 +417,15 @@ data class CameraXState(
     override fun onOrientationChanged(orientation: Int) {
         imageAnalysis?.targetRotation = when (orientation) {
             in 225 until 315 -> {
-                Surface.ROTATION_90
+                Surface.ROTATION_0
             }
 
             in 135 until 225 -> {
-                Surface.ROTATION_180
+                Surface.ROTATION_0
             }
 
             in 45 until 135 -> {
-                Surface.ROTATION_270
+                Surface.ROTATION_0
             }
 
             else -> {
